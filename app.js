2b82cbb7a7d6b5d95340425bcc0d75237db9093b
vincentcui5296 <vincentcui5296@gmail.com>
2020-08-10 01:24:11 -0600

    Creating Campgrounds

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require("express");
-
 const app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+const campgrounds = [
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goad's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+]
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-    const campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goad's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-    ]
-
     res.render("campgrounds", {campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+    const name = req.body.name;
+    const image = req.body.image;
+    const newCampground = {name, image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new");
 })
 
 app.listen(3000, () => {
