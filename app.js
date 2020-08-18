91b9be2e49fcdfbf729a8db64e6a7b96d54b03f7
vincentcui5296 <vincentcui5296@gmail.com>
2020-08-17 23:17:29 -0600

    Style Comments

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/public`));
 
 const seedDB = require("./seeds");
 seedDB();
--- seeds.js
+++ seeds.js
@@ -5,17 +5,17 @@ const data = [
     {
         name: "Cloud's Rest",
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "blah blah blah"
+        description: "Sed ornare eu augue id condimentum. Duis ultrices risus metus, a imperdiet ligula sagittis eu. Pellentesque tempus interdum dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac semper sem, sit amet convallis urna. Nunc porta et quam non commodo. Proin lacinia, tellus sed rhoncus dignissim, nunc sem imperdiet est, quis convallis orci metus nec elit. Sed eget ipsum ac quam consequat aliquet. Phasellus rutrum dolor quis dolor vestibulum, eu mattis mi tincidunt. Phasellus aliquam sit amet mi non rutrum. Nullam at porttitor risus. Vivamus et sem commodo, blandit leo lobortis, tempus nisi. Sed eu risus diam. Quisque maximus, risus id molestie accumsan, elit lacus varius orci, ut varius magna dui et magna. Proin sit amet mauris faucibus, efficitur dui euismod, consectetur mi."
     },
     {
         name: "Desert Mesa",
         image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-        description: "blah blah blah"
+        description: "Integer tempor feugiat ultrices. Suspendisse in elit tempus, condimentum eros at, fringilla magna. Vestibulum consectetur augue sem, eget porta nisi feugiat nec. Fusce efficitur dignissim nisl, in vestibulum mauris suscipit sed. Morbi congue quam in efficitur rutrum. Vivamus at nibh nisl. In ultricies diam vel erat pretium posuere. Morbi ac turpis volutpat, mattis tortor vitae, suscipit ex. Sed at eros sit amet quam posuere efficitur. In malesuada dui mauris, quis lacinia leo consectetur et. Curabitur a diam in nibh aliquet bibendum eget quis odio. Sed quis turpis id magna mollis scelerisque."
     },
     {
         name: "Canyon Floor",
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "blah blah blah"
+        description: "Praesent pretium tincidunt porta. Vestibulum tempus pulvinar eros nec facilisis. Sed ornare feugiat mattis. Nulla eleifend dolor ut lectus dignissim aliquam. Vestibulum egestas eros ac faucibus pretium. Vestibulum nibh mauris, dignissim posuere ultrices ac, hendrerit commodo tellus. Integer ullamcorper sapien quis molestie tincidunt. Proin tempor sit amet dui ut aliquam. Etiam fringilla, lectus at gravida tincidunt, ex dolor rutrum massa, sit amet ultricies orci nisl ac urna. Maecenas auctor mi consectetur erat sagittis blandit. Mauris urna elit, gravida sed laoreet et, consequat nec libero. Nam tristique et odio at dapibus. Donec ac facilisis enim. Aenean vel eros ultrices, sagittis mi sit amet, rhoncus elit. Sed porttitor urna id turpis hendrerit venenatis. Maecenas vitae tempus nulla."
     }
 ]
 
